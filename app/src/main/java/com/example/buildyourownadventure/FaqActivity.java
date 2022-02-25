cd6b524a0dfe1ebb83ecba06277aac98529bb2ef
kosman2800 <78183728+kosman2800@users.noreply.github.com>
2022-02-24 20:31:13 -0500

    FAQ/Contact Screen

--- app/src/main/java/com/example/buildyourownadventure/FaqActivity.java
+++ app/src/main/java/com/example/buildyourownadventure/FaqActivity.java
@@ -1,9 +1,14 @@
 package com.example.buildyourownadventure;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class FaqActivity extends AppCompatActivity {
 
@@ -15,6 +20,13 @@ public class FaqActivity extends AppCompatActivity {
         setContentView(R.layout.activity_faq);
 
         Log.d(TAG, "onCreate: FAQ"); //log successful launch
-        setTitle("FAQ");
+        setTitle("FAQ/Contact Us");
+    }
+    public void onClick(View v){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
     }
 }
