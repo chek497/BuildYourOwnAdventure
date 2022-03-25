10a31444a239e5db7a96222934581979f356856b
kosman2800 <78183728+kosman2800@users.noreply.github.com>
2022-03-25 15:50:47 -0400

    Update BgMusicActivity.java

--- app/src/main/java/com/example/buildyourownadventure/BgMusicActivity.java
+++ app/src/main/java/com/example/buildyourownadventure/BgMusicActivity.java
@@ -2,6 +2,7 @@ package com.example.buildyourownadventure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ public class BgMusicActivity extends AppCompatActivity {
     final String TAG = "demo"; //for tag
     ListView listView;
     MediaPlayer player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,49 +34,29 @@ public class BgMusicActivity extends AppCompatActivity {
         //Get all files in raw directory
         Field[] songs = R.raw.class.getFields();
         //add songs to list
-        for(int i = 0; i<songs.length; i++){
+        for (int i = 0; i < songs.length; i++) {
             arrayList.add(songs[i].getName());
         }
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.list_items, android.R.id.text1, arrayList);
         listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View v, int i, long l){
-                //If music is playing stop it
-                if(player != null){
-                    player.release();
-                }
-                //get resource id of sound file
+            public void onItemClick(AdapterView<?> adapterView, View v, int i, long l) {
+                Log.d(TAG, "Clicked");
+                Intent intent = new Intent(BgMusicActivity.this, MyMusicService.class);
                 int resId = getResources().getIdentifier(arrayList.get(i), "raw", getPackageName());
-                //play sound file using resource id
-                player = MediaPlayer.create(BgMusicActivity.this,resId);
-                player.start();
-                Toast.makeText(BgMusicActivity.this, "Playing " + arrayList.get(i), Toast.LENGTH_SHORT).show();
+                intent.putExtra("resId", resId);
+                startService(intent);
 
             }
         });
 
 
     }
-    public void onStop(View v){
-        if(player != null) {
-            player.pause();
-            Toast.makeText(BgMusicActivity.this, "Stopped Sound", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(BgMusicActivity.this, "Nothing is currently being played", Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void onResume(View v){
-        if(player != null) {
-            player.start();
-            Toast.makeText(BgMusicActivity.this, "Resumed Sound", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(BgMusicActivity.this, "Nothing is currently being played", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-
+    public void onStop(View v) {
+        Intent intent = new Intent(BgMusicActivity.this, MyMusicService.class);
+        stopService(intent);
+    }
 }
