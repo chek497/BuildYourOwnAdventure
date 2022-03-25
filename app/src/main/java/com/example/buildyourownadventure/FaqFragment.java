ff338e69126fe59515b0a84961943ffc77d64bc5
csweet11 <77942125+csweet11@users.noreply.github.com>
2022-03-25 15:27:40 -0400

    Added edits to Contact fragment XML
    Added edits to Contact  fragment XML

--- app/src/main/java/com/example/buildyourownadventure/FaqFragment.java
+++ app/src/main/java/com/example/buildyourownadventure/FaqFragment.java
@@ -8,40 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FaqFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FaqFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FaqFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FaqFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FaqFragment newInstance(String param1, String param2) {
+    public static FaqFragment newInstance() {
         FaqFragment fragment = new FaqFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +27,7 @@ public class FaqFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
         }
     }
 
@@ -59,6 +35,10 @@ public class FaqFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faq, container, false);
+        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+
+
+
+        return view;
     }
 }
