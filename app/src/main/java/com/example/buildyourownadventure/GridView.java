996a17013799a1bb382637d15032b2f1d116e2cd
kosman2800 <78183728+kosman2800@users.noreply.github.com>
2022-04-15 15:45:44 -0400

    Added multiple players to the map

--- app/src/main/java/com/example/buildyourownadventure/GridView.java
+++ app/src/main/java/com/example/buildyourownadventure/GridView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,12 +17,15 @@ public class GridView extends View {
         UP, DOWN, LEFT, RIGHT
     }
     private Cell[][] cells;
-    private Cell player;
+    private Cell[] players;
 
-    private static final int COLS = 7, ROWS = 10;
+    private static final int COLS = 7, ROWS = 10, PLAYER_COUNT = 5;
     private static final float WALL_THICKNESS = 4;
-    private float cellSize, hMargin, vMargin;
+    private float cellSize;
+    private float hMargin;
+    private float vMargin;
     private Paint borderPaint, playerPaint;
+
 
     public GridView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -40,12 +44,15 @@ public class GridView extends View {
 
         for(int x = 0; x<COLS; x++){
             for(int y = 0; y<ROWS; y++){
-                cells[x][y] = new Cell(x,y);
+                cells[x][y] = new Cell(x, y);
             }
         }
 
-            player = cells[0][0];
 
+            players = new Cell[PLAYER_COUNT];
+            for(int i=0;i<players.length;i++){
+                players[i] = cells[i][0];
+            }
 
     }
 
@@ -67,72 +74,67 @@ public class GridView extends View {
 
         for(int x = 0; x<COLS; x++){
             for(int y = 0; y<ROWS; y++){
-                if(cells[x][y].topWall){
-                    canvas.drawLine(
-                            x*cellSize,
-                            y*cellSize,
-                            (x+1)*cellSize,
-                            y*cellSize,
-                            borderPaint);
-                }
-                if(cells[x][y].leftWall){
-                    canvas.drawLine(
-                            x*cellSize,
-                            y*cellSize,
-                            x*cellSize,
-                            (y+1)*cellSize,
-                            borderPaint);
-                }
-                if(cells[x][y].bottomWall){
-                    canvas.drawLine(
-                            x*cellSize,
-                            (y+1)*cellSize,
-                            (x+1)*cellSize,
-                            (y+1)*cellSize,
-                            borderPaint);
-                }
-                if(cells[x][y].rightWall){
-                    canvas.drawLine(
-                            (x+1)*cellSize,
-                            y*cellSize,
-                            (x+1)*cellSize,
-                            (y+1)*cellSize,
-                            borderPaint);
-                }
+                canvas.drawLine(
+                        x*cellSize,
+                        y*cellSize,
+                        (x+1)*cellSize,
+                        y*cellSize,
+                        borderPaint);
+                canvas.drawLine(
+                        x*cellSize,
+                        y*cellSize,
+                        x*cellSize,
+                        (y+1)*cellSize,
+                        borderPaint);
+            
+                canvas.drawLine(
+                        x*cellSize,
+                        (y+1)*cellSize,
+                        (x+1)*cellSize,
+                        (y+1)*cellSize,
+                        borderPaint);
+            
+                canvas.drawLine(
+                        (x+1)*cellSize,
+                        y*cellSize,
+                        (x+1)*cellSize,
+                        (y+1)*cellSize,
+                        borderPaint);
+                
             }
         }
         float margin = cellSize/10;
-
-        canvas.drawRect(
-                player.col * cellSize + margin,
-                player.row * cellSize + margin,
-                (player.col + 1) * cellSize - margin,
-                (player.row + 1) * cellSize - margin,
-                playerPaint
-        );
+        for(Cell player:players){
+            canvas.drawRect(
+                    player.col * cellSize + margin,
+                    player.row * cellSize + margin,
+                    (player.col + 1) * cellSize - margin,
+                    (player.row + 1) * cellSize - margin,
+                    playerPaint
+            );
+        }
 
     }
-    private void movePlayer(Direction direction){
+    private void movePlayer(Direction direction, int i){
         switch (direction){
             case UP:
-                if (player.row != 0) {
-                    player = cells[player.col][player.row - 1];
+                if (players[i].row != 0) {
+                    players[i] = cells[players[i].col][players[i].row - 1];
                 }
-
                 break;
             case DOWN:
-                if (player.row != ROWS - 1) {
-                    player = cells[player.col][player.row + 1];
-                }
+                if (players[i].row != ROWS - 1) {
+                    players[i] = cells[players[i].col][players[i].row + 1];
 
+                }
                 break;
             case LEFT:
-                if(player.col != 0) {
-                    player = cells[player.col - 1][player.row];
+                if(players[i].col != 0) {
+                    players[i] = cells[players[i].col - 1][players[i].row];
                 }
                 break;
             case RIGHT:
-                player = cells[player.col+1][player.row];
+                players[i] = cells[players[i].col+1][players[i].row];
                 break;
 
         }
@@ -140,61 +142,95 @@ public class GridView extends View {
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        float x, y, dx, dy, absDx,absDy,playerCenterX,playerCenterY;
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
 
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            return true;
-        }
-        if(event.getAction() == MotionEvent.ACTION_MOVE){
-            float x = event.getX();
-            float y = event.getY();
-
-
-            float playerCenterX = hMargin + (player.col + 0.5f) * cellSize;
-            float playerCenterY = vMargin + (player.row + 0.5f) * cellSize;
+                for (Cell player : players) {
+                    x = event.getX();
+                    y = event.getY();
 
 
-            float dx = x - playerCenterX;
-            float dy = y - playerCenterY;
+                    playerCenterX = hMargin + (player.col + 0.5f) * cellSize;
+                    playerCenterY = vMargin + (player.row + 0.5f) * cellSize;
 
-            float absDx = Math.abs(dx);
-            float absDy = Math.abs(dy);
-
-
-            if (absDx > cellSize || absDy > cellSize) {
-                if (absDx > absDy) {
-                    //move in x direction
-
-                    if (dx > 0) {
-                        movePlayer(Direction.RIGHT);
-                    } else {
-                        movePlayer(Direction.LEFT);
+                    dx = x - playerCenterX;
+                    dy = y - playerCenterY;
+                    absDx = Math.abs(dx);
+                    absDy = Math.abs(dy);
+                    if (player.isTouched(absDx, absDy)) {
+                        player.setActionDown(true);
                     }
-                } else {
-                    //move in y direction
-                    if (dy > 0) {
-                        movePlayer(Direction.DOWN);
-                    } else {
-                        movePlayer(Direction.UP);
+
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                for(int i = 0; i<players.length; i++) {
+                    if (players[i].getActionDown()) {
+                        x = event.getX();
+                        y = event.getY();
+
+                        playerCenterX = hMargin + (players[i].col + 0.5f) * cellSize;
+                        playerCenterY = vMargin + (players[i].row + 0.5f) * cellSize;
+
+                        dx = x - playerCenterX;
+                        dy = y - playerCenterY;
+
+                        absDx = Math.abs(dx);
+                        absDy = Math.abs(dy);
+
+                        if (absDx > cellSize || absDy > cellSize) {
+                            if (absDx > absDy) {
+                                //move in x direction
+                                if (dx > 0) {
+                                    movePlayer(Direction.RIGHT, i);
+
+                                } else {
+                                    movePlayer(Direction.LEFT, i);
+
+                                }
+                            } else {
+                                //move in y direction
+                                if (dy > 0) {
+                                    movePlayer(Direction.DOWN, i);
+
+                                } else {
+                                    movePlayer(Direction.UP, i);
+
+                                }
+                            }
+
+                        }
                     }
                 }
-
-            }
-            return true;
+                break;
+            case MotionEvent.ACTION_UP:
+                for(Cell player:players) {
+                    player.setActionDown(false);
+                }
+                break;
         }
-        return super.onTouchEvent(event);
+        return true;
+
     }
 
-    private class Cell{
-        boolean
-            topWall = true,
-            leftWall = true,
-            bottomWall = true,
-            rightWall = true;
+    private static class Cell{
+        boolean actionDown = false;
         int col, row;
 
         public Cell(int col, int row) {
             this.col = col;
             this.row = row;
+        }
+        public boolean getActionDown(){
+            return actionDown;
+        }
+        public void setActionDown(boolean actionDown){
+            this.actionDown = actionDown;
+        }
+
+        public boolean isTouched(float x, float y) {
+            return x <= 50 && y <= 50;
         }
     }
 }
